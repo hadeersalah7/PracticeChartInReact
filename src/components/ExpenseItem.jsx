1f14676304791c29bb9f64b2c745ee4fca1da204
hadeersalah7 <hadeermhmd70@gmail.com>
2024-09-03 12:46:59 +0300

    adding first custome component

---
+++ src/components/ExpenseItem.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./ExpenseItem.css"
+function ExpenseItem(props) {
+
+    return (
+        <div className='expense-item'>
+            <div className='expense-item__description'>{props.date.toLocaleDateString()}</div>
+            <div><h2>{props.title}</h2>
+                <div className='expense-item__price'>${props.amount }</div>
+            </div>
+        </div>
+    )
+}
+
+export default ExpenseItem
