12bfec615bff9fbd95470e3076582cb4a73dbfc8
hadeersalah7 <hadeermhmd70@gmail.com>
2024-09-08 15:25:32 +0300

    adding conditional rendring

--- src/componets/Expenses/Expenses.jsx
+++ src/componets/Expenses/Expenses.jsx
@@ -1,21 +1,27 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import "./Expenses.css"
 import ExpensesFilter from '../Expenses/ExpenseFilter';
 import ExpenseItem from './ExpenseItem';
 import Card from '../../UI/Card';
-function Expenses({expenses}) {
+function Expenses({ expenses }) {
     const [filteredYear, setFilteredYear] = useState('2020');
 
     const filterChangeHandler = selectedYear => {
         setFilteredYear(selectedYear);
     };
+    
     return (
         <Card className='expenses'>
             <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-            <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
-            <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
-            <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
-            <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} />
+            {expenses.length === 0 && <p>No Expenses found.</p>}
+            {expenses.length > 0 && (
+                expenses.map(expense =>
+                <ExpenseItem key={expense.id}
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date} />)
+            )}
+            
         </Card>
     )
 }
